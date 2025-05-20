4e523fcf2aeb3669f893b63edc63cd8d18f65590
shahirahzaki <shrhmdzk@gmail.com>
2025-05-20 23:09:09 +0800

    feat: update category selection to filter meals based on selected category

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredList =
+        dummyMeals
+            .where((meal) => meal.categories.contains(category.id))
+            .toList();
+
     Navigator.push(
       //or Navigator.of(context).push(route)
       context,
       MaterialPageRoute(
-        builder: (ctx) => MealsScreen(title: 'Some title', meals: []),
+        builder:
+            (ctx) => MealsScreen(title: category.title, meals: filteredList),
       ),
     );
   }
@@ -35,7 +42,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () => _selectCategory(context, category),
             ),
         ],
       ),
