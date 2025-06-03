2f6d99eaa2a9c00ae1ea67d53f61e1aaa92f42b7
shahirahzaki <shrhmdzk@gmail.com>
2025-06-03 22:07:22 +0800

    feat: update FavouriteMealsNotifier to return bool from toggleMealFavouriteStatus and refactor MealDetailsScreen to use provider

--- lib/providers/favourites_provider.dart
+++ lib/providers/favourites_provider.dart
@@ -5,17 +5,20 @@ class FavouriteMealsNotifier extends StateNotifier<List<Meal>> {
   // <> to tell flutter what kind of data will be handled by the notifier, or provider.
   FavouriteMealsNotifier() : super([]);
 
-  void toggleMealFavouriteStatus(Meal meal) {
+  bool toggleMealFavouriteStatus(Meal meal) {
+    // will return type bool
     //expects meal of type Meal as input
     final mealIsFavourite = state.contains(meal);
 
     if (mealIsFavourite) {
       state = state.where((m) => m.id != meal.id).toList();
+      return false;
     } else {
       state = [
         ...state,
         meal,
       ]; //pulls out all existing elements in state and append meal
+      return true;
     }
   }
 }
